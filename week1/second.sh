190726af9af2c3906d1e01df83ffa3d68a31645b
magicshrimp <sampalmer3@gmail.com>
2024-07-21 02:37:24 +0000

    week2 progress

--- week1/second.sh
+++ week1/second.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-echo "Hi there!"
-echo "It's good to see you $1!"
+
+
+read -rp "Hi there! Please Enter your names: " nameOne nameTwo nameThree
+unit="CYB6004 Scripting Languages"
+
+echo -e "Hello $nameOne, $nameTwo, $nameThree! Welcome to \n\"$unit\""
+
+read -rsp "What is the secret password: " password
+
+echo "I wont tell anyone"
+
+echo "I lied! Your password was $password!"
+
 exit 0
