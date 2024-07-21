226d87dcbdeefff13680509ef097f4ff23ccb7ae
magicshrimp <sampalmer3@gmail.com>
2024-07-21 05:08:55 +0000

    week 2 progress

---
+++ week2/example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello, what is your name?"
+
+read -r name
+
+echo "Nice to meet you $name!"
+
+exit 0
---
+++ week2/setPassword2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#this asks the question about where you would like to save the password.txt
+echo "Please type the which week's folder you wish to save your password in :)"
+
+#this command asks for your input to the previous question
+read week
+
+#this line is to echo your chosen folder back to you to confirm its accuracy
+echo "Great. You have chosen $week"
+
+#here the script is asking you to type your password
+echo "Please type your password"
+
+#this is the input to the previous prompt
+read password
+
+#this is where the password is converted to a hash and saved at the desired location as a txt doc
+echo $password | sha256sum > ~/scripts/portfolio/"$week"/password.txt
+
+#states that your password document has been created and ask user to confirm that the document is where it is supposed to be
+echo "Great! Please check the desired folder directory to make sure your password has been saved!"
+
+#this command generates a successful response on completion of the script
+exit 0
