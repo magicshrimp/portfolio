dc967b59b4a334d19a1a28703f3aac82be63cf93
magicshrimp <sampalmer3@gmail.com>
2024-07-26 07:54:14 +0000

    More progress week 2

---
+++ week2/passwordCheck.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+read -sp "Enter password: " var_secret
+
+var_hash=$(echo $var_secret | sha256sum)
+
+hash_file="password.txt"
+my_variable=$(cat "$hash_file")
+
+echo "1: $var_hash"
+echo "2: $my_variable"
+
+if [ "$var_hash" == "$my_variable" ]
+    then echo "Access Granted!"
+    exit 0
+else
+    echo "Access Denied.. "
+    exit 1
+fi
+
+#***Working Out***
+
+#cat ~/scripts/portfolio/week2/password.txt savedHash
+
+#hash="$password" > sha256sum
+#cat ~/scripts/portfolio/week2/password.txt
+
+#if [ "$savedHash" = "$hash" ] ;
+#then    
+ #   echo "The password is correct. Access granted!"
+#else   
+ #   echo "The password is incorrect. Access denied.."
+#fi
+
+
+#read -sp "Enter password: " var_spass
+
+#var_hash=$(echo $var_spass | sha256sum)
+
+#hashfile="password.txt"
+#myvariable=$(cat "$hashfile")
+
+#echo "1: $var_hash"
+#echo "2: $myvariable"
+
+#if [ "$var_hash" == "$myvariable" ]
+#    then echo "Access Granted!"
+#    exit 0
+#else
+#    echo "Access Denied.. "
+#    exit 1
+#fi
